3aa25edddeb7201df23cb32deb53de43b6e2c0bb
Raj <logaraj3@gmail.com>
2025-03-24 03:00:11 +0800

    Delete repeated content

--- project_2_raj/brazilian_ecommerce_dbt/project2/models/dim_order_items.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    order_id,
-    order_item_id,
-    product_id,
-    seller_id,
-    shipping_limit_date,
-    price,
-    freight_value
-FROM {{ source('brazil_e_commerce', 'order_items') }}
--- project_2_raj/brazilian_ecommerce_dbt/project2/models/dim_payments.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    order_id,
-    payment_sequential,
-    payment_type,
-    payment_installments,
-    payment_value
-FROM {{ source('brazil_e_commerce', 'order_payments') }}
