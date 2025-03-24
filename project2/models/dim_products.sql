016b04776809ddc17d3b5f74c67a1d8a60b37254
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-03-24 22:20:11 +0800

    correct the column names when creating Dim products

--- project2/models/dim_products.sql
+++ project2/models/dim_products.sql
@@ -1,8 +1,8 @@
 SELECT
     product_id,
     product_category_name,
-    product_name_lenght,
-    product_description_lenght,
+    product_name_lenght as product_name_length,
+    product_description_lenght as product_description_length,
     product_photos_qty,
     product_weight_g,
     product_length_cm,
