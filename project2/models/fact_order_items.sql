0b61c5ba247ab9a0892a4f02cfaee5f43e3d4e1b
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-03-25 22:52:37 +0800

    Updated dbt models and tests

--- project2/models/fact_order_items.sql
+++ project2/models/fact_order_items.sql
@@ -2,8 +2,6 @@ SELECT
     order_id,
     order_item_id,
     product_id,
-    seller_id,
-    shipping_limit_date,
     price,
     freight_value
 FROM {{ source('brazil_e_commerce', 'order_items') }}
