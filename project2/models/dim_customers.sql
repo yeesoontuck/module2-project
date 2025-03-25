6f480f85d779cc8b839d218ce2d4659dd1dfd88a
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-03-25 22:52:37 +0800

    Updated dbt models and tests

--- project2/models/dim_customers.sql
+++ project2/models/dim_customers.sql
@@ -1,7 +1,6 @@
 SELECT
     customer_id,
     customer_unique_id,
-    customer_zip_code_prefix,
     customer_city,
     customer_state
 FROM {{ source('brazil_e_commerce', 'customers') }}
--- project2/models/dim_order_total_value.sql
+++
@@ -1,4 +0,0 @@
-SELECT 
-  `order_id`, ROUND(SUM(`price` + `freight_value`), 2) as `total_order_value`
-FROM {{ source('brazil_e_commerce', 'order_items') }}
-GROUP BY `order_id`
