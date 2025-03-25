0ada11c3817e0cd630645cfc315f39bd9390991d
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-03-25 12:50:34 +0800

    Modified dim_products to include the English translated category names as a new column

--- project2/models/dim_products.sql
+++ project2/models/dim_products.sql
@@ -1,11 +1,19 @@
+WITH products_cte AS (
+    SELECT
+        p.product_id,
+        p.product_category_name,
+        t.product_category_name_english,
+        p.product_name_lenght as product_name_length,
+        p.product_description_lenght as product_description_length,
+        p.product_photos_qty,
+        p.product_weight_g,
+        p.product_length_cm,
+        p.product_height_cm,
+        p.product_width_cm
+    FROM {{ source('brazil_e_commerce', 'products') }} AS p
+    LEFT JOIN {{ source('brazil_e_commerce', 'product_category_name_translation') }} AS t
+        ON p.product_category_name = t.product_category_name
+)
 SELECT
-    product_id,
-    product_category_name,
-    product_name_lenght as product_name_length,
-    product_description_lenght as product_description_length,
-    product_photos_qty,
-    product_weight_g,
-    product_length_cm,
-    product_height_cm,
-    product_width_cm
-FROM {{ source('brazil_e_commerce', 'products') }}
+    *
+FROM products_cte
