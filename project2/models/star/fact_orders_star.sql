6b630650721c1c1eae439d479419d4137dba1baa
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-03-26 12:47:09 +0800

    Updated star schema models and Notebook

--- project2/models/star/fact_orders_star.sql
+++ project2/models/star/fact_orders_star.sql
@@ -9,17 +9,17 @@
 WITH total_price AS (
     SELECT 
         `o`.`order_id`, 
-        `o`.customer_id,
-        `o`.order_status,
-        `o`.order_purchase_timestamp,
-        `o`.order_delivered_carrier_date,
-        `o`.order_delivered_customer_date,
-        `o`.order_estimated_delivery_date,
+        `o`.`customer_id`,
+        `o`.`order_status`,
+        `o`.`order_purchase_timestamp`,
+        `o`.`order_delivered_carrier_date`,
+        `o`.`order_delivered_customer_date`,
+        `o`.`order_estimated_delivery_date`,
         ROUND(SUM(`price` + `freight_value`), 2) as `total_order_value`
     FROM {{ source('brazil_e_commerce', 'order_items') }} AS oi
     INNER JOIN {{ source('brazil_e_commerce', 'orders') }} AS o
     ON oi.`order_id` = o.`order_id`
-    GROUP BY `order_id`,`o`.customer_id,`o`.order_status,`o`.order_purchase_timestamp,`o`.order_delivered_carrier_date,`o`.order_delivered_customer_date,`o`.order_estimated_delivery_date
+    GROUP BY `order_id`, `o`.`customer_id`, `o`.`order_status`, `o`.`order_purchase_timestamp`, `o`.`order_delivered_carrier_date`, `o`.`order_delivered_customer_date`, `o`.`order_estimated_delivery_date`
 )
 SELECT
     *
