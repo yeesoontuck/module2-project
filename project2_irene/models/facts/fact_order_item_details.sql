192fbeb3c959c1fa2342dd4f1e0fffe6ed0672e6
ileenluu <irenelua@singnet.com.sg>
2025-03-27 03:55:06 +0800

    modify fact_item_details.sql and delete stg sql files.

--- project2_irene/models/facts/fact_order_item_details.sql
+++ project2_irene/models/facts/fact_order_item_details.sql
@@ -1,11 +1,20 @@
 WITH order_sums AS (
+  -- Summing the total payment value at the order level
+  SELECT 
+      o.order_id,
+      SUM(op.payment_value) AS total_payment_value
+  FROM {{ source('brazilian_ecommerce', 'orders') }} o
+  LEFT JOIN {{ source('brazilian_ecommerce', 'order_payments') }} op
+  ON o.order_id = op.order_id
+  GROUP BY o.order_id
+),
+
+order_item_values AS (
+  -- Summing the total order value at the item level
   SELECT 
       oi.order_id,
-      SUM(oi.price) AS total_order_value,
-      SUM(op.payment_value) AS total_payment_value
+      SUM(oi.price) AS total_order_value
   FROM {{ source('brazilian_ecommerce', 'order_items') }} oi
-  LEFT JOIN {{ source('brazilian_ecommerce', 'order_payments') }} op
-  ON oi.order_id = op.order_id
   GROUP BY oi.order_id
 )
 
@@ -15,11 +24,13 @@ SELECT
   oi.seller_id,
   oi.product_id,
   oi.price,
-  oi.price / os.total_order_value AS proportion,
+  oi.price / oiv.total_order_value AS proportion,
+  oi.shipping_limit_date,
+  oi.freight_value,
   os.total_payment_value,
-  os.total_payment_value * (oi.price / os.total_order_value) AS prop_payment_value,
-
-
+  os.total_payment_value * (oi.price / oiv.total_order_value) AS prop_payment_value
 FROM {{ source('brazilian_ecommerce', 'order_items') }} oi
 JOIN order_sums os
-ON oi.order_id = os.order_id
+  ON oi.order_id = os.order_id
+JOIN order_item_values oiv
+  ON oi.order_id = oiv.order_id
